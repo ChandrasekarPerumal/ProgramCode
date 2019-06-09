300ab8f78e3071df898eedd375f67426795efebb
ChandrsekarPerumal <47257171+ChandrsekarPerumal@users.noreply.github.com>
2019-06-09 13:27:02 +0530

    Update Hour_Glass_Hack.cpp

--- Hour_Glass_Hack.cpp
+++ Hour_Glass_Hack.cpp
@@ -3,25 +3,28 @@ using namespace std;
 int main()
 {
     int arr_rows,arr_columns;
+   /* 
     cout<<"Rows and columns\n";
-    cin>>arr_rows>>arr_columns;
-   // int arr[arr_rows][arr_columns];
+    cin>>arr_rows>>arr_columns;       //User Input.
+    int arr[arr_rows][arr_columns];
     cout<<"Data array\n";
-   /* for(int i=0;i<arr_rows;i++)
+    for(int i=0;i<arr_rows;i++)
     {
         for(int j=0;j<arr_columns;j++)
             cin>>arr[i][j];
     }
+int arr[6][6]={{1,1,1,0,0,0},{0,1,0,0,0,0},{1,1,1,0,0,0},{0,0,2,4,4,0},{0,0,0,2,0,0},{0,0,1,2,4,0}};
 */
-   //int arr[6][6]={{1,1,1,0,0,0},{0,1,0,0,0,0},{1,1,1,0,0,0},{0,0,2,4,4,0},{0,0,0,2,0,0},{0,0,1,2,4,0}};
- int arr[6][6]= {{-1,-1,0,-9,-2,-2},{-2,-1,-6,-8,-2,-5},{-1,-1,-1,-2,-3,-4},{-1,-9,-2,-4,-4,-5},{-7,-3,-3,-2,-9,-9},{-1,-3,-1,-2,-4,-5}};
+int arr[6][6]= {{-1,-1,0,-9,-2,-2},{-2,-1,-6,-8,-2,-5},{-1,-1,-1,-2,-3,-4},{-1,-9,-2,-4,-4,-5},{-7,-3,-3,-2,-9,-9},{-1,-3,-1,-2,-4,-5}};
+ //Display the square matrix data..
     for(int i=0;i<arr_rows;i++)
     {
         cout<<"\n\t";
         for(int j=0;j<arr_columns;j++)
             cout<<arr[i][j]<<" ";
     }
-    int max1=0,tot=0;
+    int max1=-100000,tot=0;
+ //Find Maximum Sum in the Hour glass
     for(int i=0;i<arr_rows-2;i++)
     {
         for(int j=0;j<arr_columns-2;j++)
@@ -29,7 +32,7 @@ int main()
             tot=arr[i][j]+arr[i][j+1]+arr[i][j+2];
             tot+=arr[i+1][j+1];
             tot+=arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            cout<<"\n Total:=  "<<tot;
+           // cout<<"\n Total:=  "<<tot;
             if(tot>max1)
             {
                 max1=tot;
@@ -37,12 +40,12 @@ int main()
             tot=0;
         }
     }
-    cout<<"\n\t"<<max1;
+    cout<<"\n\tMaximum Sum ="<<max1;
 
 
 
 /*
-
+// Display the data as like Hour glass
     for(int i=0;i<arr_rows-2;i++)
     {
         cout<<"\n";
